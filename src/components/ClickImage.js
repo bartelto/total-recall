9e8c8b9c979493a2799aa9747e2820d696b28405
Miles Standoffish <toddnan@gmail.com>
2019-10-15 16:58:50 -0500

    game scoring functional

--- src/components/ClickImage.js
+++ src/components/ClickImage.js
@@ -2,14 +2,14 @@ import React from "react";
 
 let divStyle = (imgUrl) => {
   return { backgroundImage: `url(${imgUrl})` };
-  //return { backgroundImage: `url("./terminator-genisys.jpg")` };
 };
 
 function ClickImage(props) {
   return (
     <div className="click-image"
+      id={props.id}
       style={divStyle(props.image)}
-      /*onClick={props.handleImageClick}*/ >
+      onClick={() => props.handleImageClick(props.id)} >
     </div>
   );
 }
--- src/components/Game.js
+++ src/components/Game.js
@@ -10,26 +10,97 @@ console.log(tg);
 class Game extends React.Component {
   // Setting the initial state of the Counter component
   state = {
-    count: 0,
-    images: [tg, tr, tg, tr, tg, tr, tg, tr, tg, tr, tg, tr]
+    score: 0,
+    highScore: 0,
+    buttons: [
+      {
+        id: 1,
+        image: tg
+      }, 
+      {
+        id: 2,
+        image: tr
+      }, 
+      {
+        id: 3,
+        image: tg
+      }, 
+      {
+        id: 4,
+        image: tr
+      }, 
+      {
+        id: 5,
+        image: tg
+      }, 
+      {
+        id: 6,
+        image: tr
+      }, 
+      {
+        id: 7,
+        image: tg
+      }, 
+      {
+        id: 8,
+        image: tr
+      }, 
+      {
+        id: 9,
+        image: tg
+      }, 
+      {
+        id: 10,
+        image: tr
+      }, 
+      {
+        id: 11,
+        image: tg
+      }, 
+      {
+        id: 12,
+        image: tr
+      }
+    ],
+    unclickedIds: [] // or perhaps use image URLs?
   };
 
-  // handleIncrement increases this.state.count by 1
-  handleIncrement = () => {
-    // We always use the setState method to update a component's state
-    this.setState({ count: this.state.count + 1 });
+  // handleImageClick increases this.state.count by 1
+  handleImageClick = (id) => {
+    console.log(`clicked id ${id}`);
+    
+    if (!this.state.unclickedIds.includes(id)) {
+      // button had not been clicked before
+      this.state.unclickedIds.push(id);
+      this.setState({ score: this.state.score + 1 });
+      if (this.state.score >= this.state.highScore) {
+        // new high score
+        this.setState({ highScore: this.state.score + 1 });
+      }
+    }
+    else {
+      // button has already been clicked
+      this.setState({ 
+        score: 0, 
+        unclickedIds: [] 
+      });
+    }
   };
 
   // The render method returns the JSX that should be rendered
   render() {
     return (
       <div className="container">
+        <p>Score: {this.state.score}</p>
+        <p>High Score: {this.state.highScore}</p>
         <div className="row">
-          {this.state.images.map(image => (
+          {this.state.buttons.map(button => (
             <div className="col text-center">
               <ClickImage
-                image={image}
-                handleImageClick={this.handleIncrement}
+                key={button.id} 
+                id={button.id} 
+                image={button.image}
+                handleImageClick={this.handleImageClick}
               />
             </div>
           ))}
