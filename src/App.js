574d2a41f3bc08584c61e070e639869cbf97f94b
Miles Standoffish <toddnan@gmail.com>
2019-10-16 21:31:45 -0500

    improved styling

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Game from './components/Game.js';
-const logo2 = require('./assets/images/terminator-genisys.jpg');
-const logo3 = require('./assets/images/total-recall.png');
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          Stay tuned here for a new game from Todd!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-
+        <div class="jumbotron jumbotron-fluid">
+          <div class="container">
+            <h1>TOTAL RECALL</h1>
+            <p class="lead">Test your memory! Click all the Arnies, but don't click any more than once!</p>
+          </div>
+        </div>
       </header>
       <div>
         <Game></Game>
--- src/components/Game.js
+++ src/components/Game.js
@@ -10,10 +10,10 @@ buttons.forEach(button => {
 function shuffleArray(array) {
   console.log(array);
   for (var i = array.length - 1; i > 0; i--) {
-      var j = Math.floor(Math.random() * (i + 1));
-      var temp = array[i];
-      array[i] = array[j];
-      array[j] = temp;
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
   }
 }
 
@@ -23,32 +23,23 @@ class Game extends React.Component {
   state = {
     score: 0,
     highScore: 0,
+    message: "Click any image to begin.",
     buttons,
-    /*buttons: [
-      {
-        id: 1,
-        image: tg
-      }, 
-      {
-        id: 2,
-        image: tr
-      }, 
-      {
-        id: 3,
-        image: kc
-      }
-    ],*/
     unclickedIds: [] // or perhaps use image URLs?
   };
 
   // handleImageClick increases this.state.count by 1
   handleImageClick = (id) => {
     console.log(`clicked id ${id}`);
-    
+
     if (!this.state.unclickedIds.includes(id)) {
       // button had not been clicked before
+
       this.state.unclickedIds.push(id);
-      this.setState({ score: this.state.score + 1 });
+      this.setState({
+        score: this.state.score + 1,
+        message: "Keep going!"
+      });
       if (this.state.score >= this.state.highScore) {
         // new high score
         this.setState({ highScore: this.state.score + 1 });
@@ -56,9 +47,10 @@ class Game extends React.Component {
     }
     else {
       // button has already been clicked
-      this.setState({ 
-        score: 0, 
-        unclickedIds: [] 
+      this.setState({
+        score: 0,
+        message: "Whoops! You clicked that one already! Click an image to try again.",
+        unclickedIds: []
       });
     }
   };
@@ -67,25 +59,31 @@ class Game extends React.Component {
     shuffleArray(this.state.buttons);
     return (
       this.state.buttons.map(button => (
-      <div className="col text-center">
-        <ClickImage
-          key={button.id} 
-          id={button.id} 
-          image={button.image}
-          handleImageClick={this.handleImageClick}
-        />
-      </div>
-    )));
+        <div className="col text-center">
+          <ClickImage
+            key={button.id}
+            id={button.id}
+            image={button.image}
+            handleImageClick={this.handleImageClick}
+          />
+        </div>
+      )));
   };
 
   // The render method returns the JSX that should be rendered
   render() {
     return (
-      <div className="container">
-        <p>Score: {this.state.score}</p>
-        <p>High Score: {this.state.highScore}</p>
-        <div className="row">
-          {this.scrambleButtons()}
+      <div>
+        <div className="container-fluid">
+          <div className="row scoreboard">
+            <div className="col-6"><p>Score: {this.state.score} | High Score: {this.state.highScore}</p></div>
+            <div className="col-6"><p>{this.state.message}</p></div>
+          </div>
+        </div>
+        <div className="container">
+          <div className="row">
+            {this.scrambleButtons()}
+          </div>
         </div>
       </div>
     );
